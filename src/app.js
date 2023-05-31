dec69632ba580b5d2733827fe6a4dc96900990c3
p8prada <131833040+p8prada@users.noreply.github.com>
2023-05-31 05:20:10 +0000

    v1 generator build + counter

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big", "last"];
+  let noun = ["jogger", "racoon", "fish"];
+  let ext = [".com", ".net", ".co", ".us", ".io"];
+
+  let cont = 0;
+
+  for (let x in pronoun) {
+    for (let y in adj) {
+      for (let z in noun) {
+        for (let i in ext) {
+          console.log(pronoun[x] + adj[y] + noun[z] + ext[i]);
+          cont++;
+        }
+      }
+    }
+  }
+  console.log("--------------------\n" + cont + " domains generated.");
 };
